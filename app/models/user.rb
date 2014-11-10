cc5e4f6e2fb8b10cf24a40e97b5c11f95b7e6b90
Hor Norin <norin.hor@gmail.com>
2014-11-10 14:13:48 +0700

    add remember me to sign_in sign_out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token, :password_not_require
+
   has_many :lessons
   
   mount_uploader :avatar, AvatarUploader
@@ -8,9 +10,35 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
   validates_format_of :email, with: /[^@\s]+@[\w\-_\.]+\.\w{2,4}/i
+  validates_length_of :password, minimum: 6, unless: :password_not_require
   
   has_secure_password
   
+  def self.digest(string)
+    BCrypt::Password.create string
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    self.password_not_require = true
+    update_attributes! remember_digest: User.digest(remember_token)
+    self.password_not_require = false
+  end
+
+  def forget
+    self.password_not_require = true
+    update_attributes! remember_digest: nil
+    self.password_not_require = false
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
   def signed_up_at
     created_at.strftime "%h %d, %Y %l:%M %p"
   end
